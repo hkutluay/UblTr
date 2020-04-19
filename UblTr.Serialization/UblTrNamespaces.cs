0a09acbb256d7f0e43c3ffb17085b1573fa3f6a1
Hakan Kutluay <hkutluay@hotmail.com>
2020-04-19 21:03:16 +0300

    Serialization project added

---
+++ UblTr.Serialization/UblTrNamespaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace UblTr.Serialization
+{
+    public class UblTrNamespaces : XmlSerializerNamespaces
+    {
+        public UblTrNamespaces()
+        {
+            Add("cac", "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2");
+            Add("cbc", "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+            Add("ccts", "urn:un:unece:uncefact:documentation:2");
+            Add("ds", "http://www.w3.org/2000/09/xmldsig#");
+            Add("ext", "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2");
+            Add("ubltr", "urn:oasis:names:specification:ubl:schema:xsd:TurkishCustomizationExtensionComponents");
+            Add("qdt", "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2");
+            Add("udt", "urn:un:unece:uncefact:data:specification:UnqualifiedDataTypesSchemaModule:2");
+            Add("xades", "http://uri.etsi.org/01903/v1.3.2#");
+            Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        }
+    }
+}
--- UblTr.Tests/InvoiceTypeTest.cs
+++ UblTr.Tests/InvoiceTypeTest.cs
@@ -3,6 +3,9 @@ using System.IO;
 using System.Xml.Serialization;
 using UblTr.MainDoc;
 using System.Linq;
+using System;
+using System.Text;
+using UblTr.Serialization;
 
 namespace UblTr.Tests
 {
@@ -50,5 +53,25 @@ namespace UblTr.Tests
             Assert.AreEqual("1288331521", invoice.Signature[0].SignatoryParty.PartyIdentification.FirstOrDefault().ID.Value);
             Assert.AreEqual(12, invoice.InvoiceLine.FirstOrDefault().InvoicedQuantity.Value);
         }
+
+        [TestMethod]
+        public void InvoiceType_BasicInvoice_Serialize()
+        {
+            var invoice = new InvoiceType();
+            invoice.UUID = new Common.UUIDType() { Value = Guid.NewGuid().ToString() };
+            invoice.UBLVersionID = new Common.UBLVersionIDType() { Value = "2.1" };
+            invoice.CustomizationID = new Common.CustomizationIDType() { Value = "TR1.2" };
+            invoice.ProfileID = new Common.ProfileIDType() { Value = "TEMELFATURA" };
+            invoice.ID = new Common.IDType() { Value = "INV20200000000001" };
+            invoice.CopyIndicator = new Common.CopyIndicatorType() { Value = false };
+
+
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(InvoiceType));
+            TextWriter writer = new StreamWriter(@"C:\Temp\TestInvoice.xml");
+            xmlSerializer.Serialize(writer, invoice, new UblTrNamespaces());
+
+        }
+
+
     }
 }
