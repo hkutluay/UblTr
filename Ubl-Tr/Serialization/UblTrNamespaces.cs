98c11d901621b722f0af21aafee94da47fcb3621
hkutluay <hkutluay@hotmail.com>
2022-05-13 21:49:24 +0300

    fixes #56

--- Ubl-Tr/Serialization/UblTrNamespaces.cs
+++ Ubl-Tr/Serialization/UblTrNamespaces.cs
@@ -1,4 +1,5 @@
-﻿using System.Xml.Serialization;
+﻿using System.Xml;
+using System.Xml.Serialization;
 
 namespace UblTr.Serialization
 {
@@ -17,5 +18,23 @@ namespace UblTr.Serialization
             Add("xades", "http://uri.etsi.org/01903/v1.3.2#");
             Add("xsi", "http://www.w3.org/2001/XMLSchema-instance");
         }
+    }
+
+    public class UblTrNamespacesManager : XmlNamespaceManager
+    {
+        public UblTrNamespacesManager(XmlNameTable TableName): base(TableName)
+        {
+            AddNamespace("cac", "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2");
+            AddNamespace("cbc", "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+            AddNamespace("ccts", "urn:un:unece:uncefact:documentation:2");
+            AddNamespace("ds", "http://www.w3.org/2000/09/xmldsig#");
+            AddNamespace("ext", "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2");
+            AddNamespace("ubltr", "urn:oasis:names:specification:ubl:schema:xsd:TurkishCustomizationExtensionComponents");
+            AddNamespace("qdt", "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2");
+            AddNamespace("udt", "urn:un:unece:uncefact:data:specification:UnqualifiedDataTypesSchemaModule:2");
+            AddNamespace("xades", "http://uri.etsi.org/01903/v1.3.2#");
+            AddNamespace("xsi", "http://www.w3.org/2001/XMLSchema-instance");
+        }
+
     }
 }
