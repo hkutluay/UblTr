67d51bff2eea3fb98c3c836d55be4f3d6dffc8c6
hkutluay <hkutluay@hotmail.com>
2023-12-26 19:06:31 +0300

    enhance time parsing to support additional formats

--- Ubl-Tr/Common/UnqualifiedDataTypes/TimeType.cs
+++ Ubl-Tr/Common/UnqualifiedDataTypes/TimeType.cs
@@ -41,17 +41,6 @@ namespace UblTr.Common
     [System.Xml.Serialization.XmlTypeAttribute(Namespace = "urn:oasis:names:specification:ubl:schema:xsd:UnqualifiedDataTypes-2")]
     public partial class TimeType
     {
-
-        private List<string> applicableTimeFormats = new List<string> {
-            "HH:mm:ss",
-            "HH:mm",
-            "HH:mm:ss.fffff",
-            "HH:mm:ss.fffffff",
-            "HH:mm:ss.fffffffzzz",
-            "HH:mm:ss.fffzzz",
-            "HH:mm:ss.fffffffZ"
-        };
-
         private System.DateTime valueField;
 
         /// <remarks/>
@@ -77,13 +66,11 @@ namespace UblTr.Common
                 bool parsed = false;
                 DateTime parsedValue;
 
-                foreach (var format in applicableTimeFormats)
+                 
+                if(DateTime.TryParse(value,CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedValue)) 
                 {
-                    if(DateTime.TryParseExact(value, format, CultureInfo.InvariantCulture, DateTimeStyles.None, out parsedValue)) 
-                    {
-                        this.valueField = parsedValue;
-                        parsed = true;
-                    }
+                    this.valueField = parsedValue;
+                    parsed = true;
                 }
                 
                 if(!parsed) 
